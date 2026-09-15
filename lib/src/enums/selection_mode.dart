be35a1876a4ad049af5fd7dd2823872ab3eb95bd
Ravi Patel <100913025+ravipatel1999@users.noreply.github.com>
2026-09-15 11:33:15 +0530

    Delete lib/src directory

--- lib/src/enums/selection_mode.dart
+++
@@ -1,8 +0,0 @@
-/// Defines whether the dropdown allows single or multiple item selection.
-enum SelectionMode {
-  /// Single item selection mode.
-  single,
-
-  /// Multiple items selection mode.
-  multiple,
-}
